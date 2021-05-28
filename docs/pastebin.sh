9dec1f46a11dff08524084aac809ab21ec5d7bdc
crosssenses <lari_wu@gmx.de>
2021-05-28 19:43:25 +0200

    Changes made on old graphite

---
+++ docs/pastebin.sh
@@ -0,0 +1,22 @@
+# Initiate and install
+
+- make new Respository on github online
+- clone repository
+- copy sample-paper-en und Ppipfile from sample-journal into new repository
+
+$ pipenv install
+
+
+# Start pipenv (in journal repo)
+
+$ pipenv shell
+
+
+# Start paper
+
+$ python3 subfolder-paper/manage.py runserver
+
+
+# Leave virtual environment
+
+Control + D
--- graphite_paper/horst/report.py
+++ graphite_paper/horst/report.py
@@ -67,6 +67,7 @@ class Report:
             if meta_name[-4:] == "yaml":
                 meta.update(yaml.safe_load(file_content))
         meta["abstract_md"] = markdown_helper(meta.get("abstract", ""))
+        meta["cite_as_md"] = markdown_helper(meta.get("cite_as", ""))
         meta["slug"] = "/%s/" % slug if slug != "index" else "/"
         self.meta = meta
     
