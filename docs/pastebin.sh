43146d1892044c976ba9addbb00d02e476908292
crosssenses <lari_wu@gmx.de>
2021-06-11 09:37:58 +0200

    Documentation

--- docs/pastebin.sh
+++ docs/pastebin.sh
@@ -20,3 +20,18 @@ $ python3 subfolder-paper/manage.py runserver
 # Leave virtual environment
 
 Control + D
+
+
+# Render docs
+
+– Start pipenv of another journal (e.g., GDHRNet)
+– go back to graphite-paper
+
+Continuous deployment for docs:
+
+$ sphinx-autobuild docs docs/_build/html
+
+Build docs
+
+$ cd docs
+$ make html
